917c4e2243f2f6119470a7880ebdb63495411f02
github@pureftpd.org <github@pureftpd.org>
2012-03-12 12:09:36 -0700

    Sync libuv up to db413f3806356a632774b8a589546fc6788debd6

--- src/libuv/src/win/error.c
+++ src/libuv/src/win/error.c
@@ -78,6 +78,8 @@ uv_err_code uv_translate_sys_error(int sys_errno) {
     case WSAEAFNOSUPPORT:                   return UV_EAFNOSUPPORT;
     case WSAEWOULDBLOCK:                    return UV_EAGAIN;
     case WSAEALREADY:                       return UV_EALREADY;
+    case ERROR_LOCK_VIOLATION:              return UV_EBUSY;
+    case ERROR_SHARING_VIOLATION:           return UV_EBUSY;
     case ERROR_CONNECTION_ABORTED:          return UV_ECONNABORTED;
     case WSAECONNABORTED:                   return UV_ECONNABORTED;
     case ERROR_CONNECTION_REFUSED:          return UV_ECONNREFUSED;
@@ -89,6 +91,8 @@ uv_err_code uv_translate_sys_error(int sys_errno) {
     case WSAEFAULT:                         return UV_EFAULT;
     case ERROR_HOST_UNREACHABLE:            return UV_EHOSTUNREACH;
     case WSAEHOSTUNREACH:                   return UV_EHOSTUNREACH;
+    case ERROR_OPERATION_ABORTED:           return UV_EINTR;
+    case WSAEINTR:                          return UV_EINTR;
     case ERROR_INVALID_DATA:                return UV_EINVAL;
     case WSAEINVAL:                         return UV_EINVAL;
     case ERROR_CANT_RESOLVE_FILENAME:       return UV_ELOOP;
@@ -102,6 +106,7 @@ uv_err_code uv_translate_sys_error(int sys_errno) {
     case ERROR_OUTOFMEMORY:                 return UV_ENOMEM;
     case ERROR_NOT_CONNECTED:               return UV_ENOTCONN;
     case WSAENOTCONN:                       return UV_ENOTCONN;
+    case ERROR_DIR_NOT_EMPTY:               return UV_ENOTEMPTY;
     case ERROR_NOT_SUPPORTED:               return UV_ENOTSUP;
     case ERROR_INSUFFICIENT_BUFFER:         return UV_EINVAL;
     case ERROR_INVALID_FLAGS:               return UV_EBADF;
