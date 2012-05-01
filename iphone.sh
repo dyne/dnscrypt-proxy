9a9ca5e9e4fc843d2217adb9ddfb808075e3b434
github@pureftpd.org <github@pureftpd.org>
2012-05-01 13:54:54 -0700

    Switch the iOS SDK to 5.1

--- iphone.sh
+++ iphone.sh
@@ -3,7 +3,7 @@
 export XCODEDIR="/Applications/Xcode.app/Contents/Developer"
 export BASEDIR="${XCODEDIR}/Platforms/iPhoneOS.platform/Developer"
 export PATH="${BASEDIR}/usr/bin:$BASEDIR/usr/sbin:$PATH"
-export SDK="${BASEDIR}/SDKs/iPhoneOS5.0.sdk"
+export SDK="${BASEDIR}/SDKs/iPhoneOS5.1.sdk"
 export CFLAGS="-pthread -mthumb -arch armv6 -isysroot ${SDK}"
 export LDFLAGS="-pthread -mthumb -arch armv6 -isysroot ${SDK}"
 
