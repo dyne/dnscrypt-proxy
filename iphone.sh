f90365bb63b9b4c10af98ba9ce97a38bba9c2cb1
github@pureftpd.org <github@pureftpd.org>
2012-05-06 15:16:48 -0700

    Update Xcode Path to 4.4DP4, and mention that iOS binaries have to be signed.

--- iphone.sh
+++ iphone.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-export XCODEDIR="/Applications/Xcode.app/Contents/Developer"
+export XCODEDIR="/Applications/Xcode44-DP4.app/Contents/Developer"
 export BASEDIR="${XCODEDIR}/Platforms/iPhoneOS.platform/Developer"
 export PATH="${BASEDIR}/usr/bin:$BASEDIR/usr/sbin:$PATH"
 export SDK="${BASEDIR}/SDKs/iPhoneOS5.1.sdk"
@@ -8,3 +8,5 @@ export CFLAGS="-pthread -mthumb -arch armv6 -isysroot ${SDK}"
 export LDFLAGS="-pthread -mthumb -arch armv6 -isysroot ${SDK}"
 
 ./configure --host=arm-apple-darwin10 && make -j2
+
+echo 'Now, using codesign(1) to sign dnscrypt-proxy'
