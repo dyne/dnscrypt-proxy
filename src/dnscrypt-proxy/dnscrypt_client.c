d03e464030cf82a9f679db2bcf3bae4fadd8612f
Frank Denis <github@pureftpd.org>
2012-05-25 20:49:13 -0700

    Trim padding from replies. Not supported by OpenDNS yet, so #ifdef'd.

--- src/dnscrypt-proxy/dnscrypt_client.c
+++ src/dnscrypt-proxy/dnscrypt_client.c
@@ -128,6 +128,14 @@ dnscrypt_client_uncurve(const DNSCryptClient * const client,
         return -1;
     }
     memset(nonce, 0, sizeof nonce);
+    assert(len >= DNSCRYPT_SERVER_BOX_OFFSET + crypto_box_BOXZEROBYTES);
+#ifdef TRIM_PADDING_FROM_REPLIES
+    assert(buf[DNSCRYPT_SERVER_BOX_OFFSET + crypto_box_BOXZEROBYTES] != 0U);
+    while (buf[--len] == 0U) { }
+    if (buf[len] != 0x80) {
+        return -1;
+    }
+#endif
     *lenp = len - (DNSCRYPT_SERVER_BOX_OFFSET + crypto_box_BOXZEROBYTES);
     memmove(buf,
             buf + DNSCRYPT_SERVER_BOX_OFFSET + crypto_box_BOXZEROBYTES, *lenp);
