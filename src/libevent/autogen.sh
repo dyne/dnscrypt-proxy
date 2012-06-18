5e17aec6d0feadcaa0a4edb1cca034de32c72333
Frank Denis <github@pureftpd.org>
2012-06-18 08:23:26 -0700

    Run libevent's autogen.sh along with the top-level one.

--- src/libevent/autogen.sh
+++ src/libevent/autogen.sh
@@ -3,13 +3,8 @@ if [ -x "`which autoreconf 2>/dev/null`" ] ; then
    exec autoreconf -ivf
 fi
 
-LIBTOOLIZE=libtoolize
-SYSNAME=`uname`
-if [ "x$SYSNAME" = "xDarwin" ] ; then
-  LIBTOOLIZE=glibtoolize
-fi
+$LIBTOOLIZE && \
 aclocal -I m4 && \
-	autoheader && \
-	$LIBTOOLIZE && \
-	autoconf && \
-	automake --add-missing --force-missing --copy
+autoheader && \
+automake --add-missing --force-missing --include-deps && \
+autoconf -I m4
