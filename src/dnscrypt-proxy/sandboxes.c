11bf5380ca4a5b607ee16baf44268215fdc6d146
Frank Denis <github@pureftpd.org>
2012-07-16 12:41:46 -0700

    Run sandbox_init() only if it's not a stub.

--- src/dnscrypt-proxy/sandboxes.c
+++ src/dnscrypt-proxy/sandboxes.c
@@ -37,7 +37,8 @@ sandboxes_pidproc(void)
 #ifdef HAVE_SANDBOX_INIT
     char *errmsg;
 
-    if (sandbox_init(kSBXProfileNoNetwork, SANDBOX_NAMED, &errmsg) != 0) {
+    if (sandbox_init != NULL &&
+        sandbox_init(kSBXProfileNoNetwork, SANDBOX_NAMED, &errmsg) != 0) {
         return -1;
     }
 #endif
