d95933037bea069770dc783c51db35a962a863ac
Frank Denis <github@pureftpd.org>
2012-07-16 18:01:31 -0700

    Update Homebrew formula.

--- packages/homebrew/dnscrypt-proxy.rb
+++ packages/homebrew/dnscrypt-proxy.rb
@@ -4,7 +4,7 @@ class DnscryptProxy < Formula
   url 'https://github.com/downloads/opendns/dnscrypt-proxy/dnscrypt-proxy-0.11.tar.gz'
   head 'https://github.com/opendns/dnscrypt-proxy.git', :branch => 'master'
   homepage 'http://www.opendns.com/technology/dnscrypt'
-  md5 '660ff9717a9fdb0fd56c0c8f989da31f'
+  md5 'c9ce5541261e2446d6ed7cfc2cbd3639'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
