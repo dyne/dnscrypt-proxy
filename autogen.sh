fa2704f0ac00f4d7c8b128c9abbb02beab02c9e3
Frank Denis <github@pureftpd.org>
2012-07-17 20:32:30 -0700

    Add libltdl

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 if [ -x "`which autoreconf 2>/dev/null`" ] ; then
-   exec autoreconf -ivf
+  exec autoreconf -ivf
 fi
 
 if glibtoolize --version > /dev/null 2>&1; then
@@ -13,10 +13,10 @@ fi
 src/libevent/autogen.sh &
 cpid=$!
 
-$LIBTOOLIZE && \
-aclocal -I m4 && \
+$LIBTOOLIZE --ltdl && \
+aclocal && \
 autoheader && \
 automake --add-missing --force-missing --include-deps && \
-autoconf -I m4
+autoconf
 
 wait $cpid
