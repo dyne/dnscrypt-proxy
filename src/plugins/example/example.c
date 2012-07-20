1a8d4a7a612d53d81bd6982168f1cfbf694c570d
Frank Denis <github@pureftpd.org>
2012-07-19 21:49:41 -0700

    Ignore unused arguments, in order to keep the example as small as possible.

--- src/plugins/example/example.c
+++ src/plugins/example/example.c
@@ -3,34 +3,22 @@
 
 int dcplugin_init(DCPlugin * const dcplugin, int argc, char *argv[])
 {
-    (void) dcplugin;
-    (void) argc;
-    (void) argv;
-
     return 0;
 }
 
 int dcplugin_destroy(DCPlugin * const dcplugin)
 {
-    (void) dcplugin;
-
     return 0;
 }
 
 DCPluginSyncFilterResult
 dcplugin_sync_pre_filter(DCPlugin *dcplugin, DCPluginDNSPacket *dcp_packet)
 {
-    (void) dcplugin;
-    (void) dcp_packet;
-
     return DCP_SYNC_FILTER_RESULT_OK;
 }
 
 DCPluginSyncFilterResult
 dcplugin_sync_post_filter(DCPlugin *dcplugin, DCPluginDNSPacket *dcp_packet)
 {
-    (void) dcplugin;
-    (void) dcp_packet;
-
     return DCP_SYNC_FILTER_RESULT_OK;
 }
