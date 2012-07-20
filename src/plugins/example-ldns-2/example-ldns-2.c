f1fa60005b0d06e0298ab66bc6b73285f28f486e
Frank Denis <github@pureftpd.org>
2012-07-20 12:12:15 -0700

    Add a second sample plugin

--- src/plugins/example-ldns-2/example-ldns-2.c
+++ src/plugins/example-ldns-2/example-ldns-2.c
@@ -15,31 +15,25 @@ DCPluginSyncFilterResult
 dcplugin_sync_pre_filter(DCPlugin *dcplugin, DCPluginDNSPacket *dcp_packet)
 {
     uint8_t  *new_packet;
-    ldns_rdf *bing, *google, *owner;
+    ldns_rdf *edns_data;
     ldns_pkt *packet;
-    ldns_rr  *question;
     size_t    new_packet_size;
 
     ldns_wire2pkt(&packet, dcplugin_get_wire_data(dcp_packet),
                   dcplugin_get_wire_data_len(dcp_packet));
 
-    question = ldns_rr_list_rr(ldns_pkt_question(packet), 0U);
-    owner = ldns_rr_owner(question);
+    edns_data = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_HEX,
+                                     "0004000f4f70656e444e53"
+                                     "0102030405060708");
+    ldns_pkt_set_edns_data(packet, edns_data);
 
-    bing = ldns_dname_new_frm_str("*.bing.com.");
-    if (ldns_dname_match_wildcard(owner, bing)) {
-        google = ldns_dname_new_frm_str("www.google.com.");
-        ldns_rdf_free(owner);
-        ldns_rr_set_owner(question, google);
-        ldns_pkt2wire(&new_packet, packet, &new_packet_size);
-        if (dcplugin_get_wire_data_max_len(dcp_packet) >= new_packet_size) {
-            dcplugin_set_wire_data(dcp_packet, new_packet, new_packet_size);
-        }
-        free(new_packet);
+    ldns_pkt2wire(&new_packet, packet, &new_packet_size);
+    if (dcplugin_get_wire_data_max_len(dcp_packet) >= new_packet_size) {
+        dcplugin_set_wire_data(dcp_packet, new_packet, new_packet_size);
     }
 
+    free(new_packet);
     ldns_pkt_free(packet);
-    ldns_rdf_free(bing);
 
     return DCP_SYNC_FILTER_RESULT_OK;
 }
