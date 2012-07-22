2036814dc592322dcec2b83515c8592bbd63d42d
Frank Denis <github@pureftpd.org>
2012-07-21 18:06:10 -0700

    Threads? We don't need threads any more.

--- iphone.sh
+++ iphone.sh
@@ -4,8 +4,8 @@ export XCODEDIR="/Applications/Xcode45-DP3.app/Contents/Developer"
 export BASEDIR="${XCODEDIR}/Platforms/iPhoneOS.platform/Developer"
 export PATH="${BASEDIR}/usr/bin:$BASEDIR/usr/sbin:$PATH"
 export SDK="${BASEDIR}/SDKs/iPhoneOS6.0.sdk"
-export CFLAGS="-Oz -pthread -mthumb -arch armv7 -isysroot ${SDK}"
-export LDFLAGS="-pthread -mthumb -arch armv7 -isysroot ${SDK}"
+export CFLAGS="-Oz -mthumb -arch armv7 -isysroot ${SDK}"
+export LDFLAGS="-mthumb -arch armv7 -isysroot ${SDK}"
 
 ./configure --host=arm-apple-darwin10 && make -j3
 
