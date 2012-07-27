2ac78d99a3ba9c113bc7146284dc159d4eda0949
Frank Denis <github@pureftpd.org>
2012-07-26 21:29:10 -0700

    Bring libevent patches up to a0912e32068621eb776d678224e4108511d281e3
        Check more setsockopt return values when binding sockets. Found by coverity
    
        memset sockaddr_in before using it. Found by coverity.
    
        Move assignment outside tt_assert in ssl unit tests. Appeases coverity.
    
        Add checks to various return values in unit tests. Found by coverity
    
        Avoid possible needless call to writev. Found by coverity.
    
        Remove unused variable; spotted by coverity
    
        Avoid more crashes/bad calls in unit tests; found by coverity
    
        Add some missing null checks to unit tests; found by coverity
    
        Fix resource leaks in the unit tests; found by coverity
    
        Fix various check-after-dereference issues in unit tests: found by coverity
    
        Set umask before calling mkstemp in unit tests. Found by coverity
    
        Fix possible uninitialized read in dns regression tests. Found by coverity.
    
        Remove deadcode in http regression tests. Found by coverity.

--- src/libevent/test/regress_main.c
+++ src/libevent/test/regress_main.c
@@ -58,6 +58,9 @@
 #endif
 
 #include <sys/types.h>
+#ifdef _EVENT_HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#endif
 
 #ifndef WIN32
 #include <sys/socket.h>
@@ -123,6 +126,9 @@ regress_make_tmpfile(const void *data, size_t datalen)
 	char tmpfilename[32];
 	int fd;
 	strcpy(tmpfilename, "/tmp/eventtmp.XXXXXX");
+#ifdef _EVENT_HAVE_UMASK
+	umask(0077);
+#endif
 	fd = mkstemp(tmpfilename);
 	if (fd == -1)
 		return (-1);
@@ -299,7 +305,7 @@ static void *
 legacy_test_setup(const struct testcase_t *testcase)
 {
 	struct basic_test_data *data = basic_test_setup(testcase);
-	if (data == (void*)TT_SKIP)
+	if (data == (void*)TT_SKIP || data == NULL)
 		return data;
 	global_base = data->base;
 	pair[0] = data->pair[0];
