0b76c63ba2db3d1bee11be3368e526d9cbe51e5a
Frank Denis <github@pureftpd.org>
2012-08-27 11:04:07 -0700

    Add two new OpenDNS-specific example plugins

---
+++ src/plugins/example-ldns-opendns-bypass/example-ldns-opendns-bypass.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#include <dnscrypt/plugin.h>
+#include <ldns/ldns.h>
+
+int
+dcplugin_init(DCPlugin * const dcplugin, int argc, char *argv[])
+{
+    return 0;
+}
+
+DCPluginSyncFilterResult
+dcplugin_sync_pre_filter(DCPlugin *dcplugin, DCPluginDNSPacket *dcp_packet)
+{
+    uint8_t  *new_packet;
+    ldns_rdf *edns_data;
+    ldns_pkt *packet;
+    size_t    new_packet_size;
+
+    ldns_wire2pkt(&packet, dcplugin_get_wire_data(dcp_packet),
+                  dcplugin_get_wire_data_len(dcp_packet));
+
+    edns_data = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_HEX,
+                                     "000400084f70656e444e53"
+                                     "00");
+    ldns_pkt_set_edns_data(packet, edns_data);
+
+    ldns_pkt2wire(&new_packet, packet, &new_packet_size);
+    if (dcplugin_get_wire_data_max_len(dcp_packet) >= new_packet_size) {
+        dcplugin_set_wire_data(dcp_packet, new_packet, new_packet_size);
+    }
+
+    free(new_packet);
+    ldns_pkt_free(packet);
+
+    return DCP_SYNC_FILTER_RESULT_OK;
+}
