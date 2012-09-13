4e8a98e7309a8d63d7694ed42d58a9656e40576a
Frank Denis <github@pureftpd.org>
2012-09-13 13:13:16 -0700

    Import a script to build the proxy on Win32

---
+++ msys.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+export CFLAGS="-fdata-sections -ffunction-sections -Os
+               -fomit-frame-pointer -march=pentium4 -mtune=core2"
+
+export LDFLAGS="-Wl,--gc-sections"
+
+./configure --disable-ssp && make -j3 install-strip
+
+upx --best --ultra-brute /usr/local/sbin/dnscrypt-proxy.exe &
+upx --best --ultra-brute /usr/local/bin/hostip.exe
+
+wait
