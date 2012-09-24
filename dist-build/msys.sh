e588bd614d20e708debca4c8b28ba9d9a9e95ade
Frank Denis <github@pureftpd.org>
2012-09-24 17:24:40 +0200

    Enable support for plugins in Windows builds.

--- dist-build/msys.sh
+++ dist-build/msys.sh
@@ -3,7 +3,8 @@
 export CFLAGS="-Os -fomit-frame-pointer -march=pentium4 -mtune=core2"
 export LDFLAGS="-Wl,--gc-sections"
 
-./configure --disable-ssp && make -j3 install-strip
+./configure --disable-ssp --enable-plugins --with-included-ltdl && \
+  make -j3 install-strip
 
 upx --best --ultra-brute /usr/local/sbin/dnscrypt-proxy.exe &
 upx --best --ultra-brute /usr/local/bin/hostip.exe
