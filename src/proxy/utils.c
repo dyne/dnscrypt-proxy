4758a1f2de4181ab438cc0891adc889bb3f65baa
Frank Denis <github@pureftpd.org>
2013-01-21 11:54:19 -0800

    Use SecureZeroMemory() on Windows

--- src/proxy/utils.c
+++ src/proxy/utils.c
@@ -16,12 +16,16 @@
 void
 dnscrypt_memzero(void * const pnt, const size_t size)
 {
+#ifdef _WIN32
+    SecureZeroMemory(pnt, size);
+#else
     volatile unsigned char *pnt_ = (volatile unsigned char *) pnt;
     size_t                     i = (size_t) 0U;
 
     while (i < size) {
         pnt_[i++] = 0U;
     }
+#endif
 }
 
 uint64_t
