e55067915849cff572d366ed08151d0a848ff8b7
Frank Denis <github@pureftpd.org>
2013-06-22 21:36:46 -0700

    Sync Homebrew formula

--- packages/homebrew/dnscrypt-proxy.rb
+++ packages/homebrew/dnscrypt-proxy.rb
@@ -2,27 +2,28 @@ require 'formula'
 
 class DnscryptProxy < Formula
   homepage 'http://dnscrypt.org'
-  url 'http://download.dnscrypt.org/dnscrypt-proxy/dnscrypt-proxy-1.2.0.tar.gz'
+  url 'http://download.dnscrypt.org/dnscrypt-proxy/dnscrypt-proxy-1.3.0.tar.bz2'
+  sha256 '211ee2d75acd631b09d012229c73654c2302234d73c9f12425e1c906520dc7c5'
+
   head 'https://github.com/opendns/dnscrypt-proxy.git', :branch => 'master'
-  sha256 '02ae6360887995d73d4c02ea7fa0cc8cad4a4de61f89c2fd68674a65f427b333'
+
+  option "plugins", "Support plugins and install example plugins."
 
   if build.head?
     depends_on :automake
     depends_on :libtool
   end
 
-  option "plugins", "Support plugins and install example plugins."
-
   def install
     system "autoreconf", "-if" if build.head?
 
-    configure_args = [ "--prefix=#{prefix}", "--disable-dependency-tracking" ]
+    args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
     if build.include? "plugins"
-      configure_args << "--enable-plugins"
-      configure_args << "--enable-plugins-root"
-      configure_args << "--enable-relaxed-plugins-permissions"
+      args << "--enable-plugins"
+      args << "--enable-relaxed-plugins-permissions"
+      args << "--enable-plugins-root"
     end
-    system "./configure", *configure_args
+    system "./configure", *args
     system "make install"
   end
 end
