c99d10eb00e2cff145f7e355f8db31a8a848c8c1
Frank Denis <github@pureftpd.org>
2013-09-15 13:46:13 -0700

    msys: actually honor prefix

--- dist-build/msys.sh
+++ dist-build/msys.sh
@@ -3,8 +3,10 @@
 export CFLAGS="-Os -march=pentium2 -mtune=nocona"
 export PREFIX="$(pwd)/dnscrypt-proxy-win32"
 
-./configure --enable-plugins --with-included-ltdl && \
-  make install-strip
+./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
+  --enable-plugins \
+  --with-included-ltdl && \
+make install-strip
 
 upx --best --ultra-brute /usr/local/sbin/dnscrypt-proxy.exe &
 upx --best --ultra-brute /usr/local/bin/hostip.exe
