536abb6fc5f9b883d25bf8453d6f208b19d50343
Frank Denis <github@pureftpd.org>
2014-05-05 17:00:20 -0700

    Ship libwinpthread-1.dll on Windows

--- dist-build/msys2-win32.sh
+++ dist-build/msys2-win32.sh
@@ -2,7 +2,7 @@
 
 export CFLAGS="-Os -m32 -march=pentium2 -mtune=nocona"
 export PREFIX="$(pwd)/dnscrypt-proxy-win32"
-export LDNS_PREFIX='/usr/mingw32'
+export MINGW_PREFIX='/usr/mingw32'
 export SODIUM_PREFIX='/tmp/libsodium-win32'
 
 export CPPFLAGS="-I${SODIUM_PREFIX}/include"
@@ -19,7 +19,8 @@ rm -fr "${PREFIX}/share"
 rm -fr "${PREFIX}/lib/pkgconfig"
 mv "${PREFIX}/lib/dnscrypt-proxy" "${PREFIX}/plugins/"
 rm -fr "${PREFIX}/lib"
-cp "${LDNS_PREFIX}/bin/libldns-1.dll" "${PREFIX}/bin/"
+cp "${MINGW_PREFIX}/bin/libwinpthread-1.dll" "${PREFIX}/bin/"
+cp "${MINGW_PREFIX}/bin/libldns-1.dll" "${PREFIX}/bin/"
 cp "${SODIUM_PREFIX}/bin/libsodium-4.dll" "${PREFIX}/bin/"
 
 cp dnscrypt-resolvers.csv "${PREFIX}/bin/"
