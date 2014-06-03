4d79a69f5bc5f5ebc415150a772e12c0770f65c8
Frank Denis <github@pureftpd.org>
2014-06-02 20:36:59 -0700

    Tweak OSX compile flags

--- dist-build/osx.sh
+++ dist-build/osx.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-export CFLAGS="-mmacosx-version-min=10.8"
-export LDFLAGS="-mmacosx-version-min=10.8"
+export CFLAGS="-mmacosx-version-min=10.8 -march=core2 -O2 -g"
+export LDFLAGS="-mmacosx-version-min=10.8 -march=core2 -O2 -g"
 
 ./configure --with-included-ltdl \
             --enable-plugins \
