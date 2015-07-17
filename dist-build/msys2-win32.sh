0fd9908270de1e1d3041d0abd1e0984be86f8cfd
Frank Denis <github@pureftpd.org>
2015-07-18 01:39:15 +0200

    Move pretty much everything in the same directory on Windows This is a breaking change, so bump major version.

--- dist-build/msys2-win32.sh
+++ dist-build/msys2-win32.sh
@@ -10,23 +10,23 @@ export LDFLAGS="-L${SODIUM_PREFIX}/lib"
 
 ./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
   --host=i686-w64-mingw32 \
-  --sbindir="${PREFIX}/bin" \
+  --sbindir="$PREFIX" \
   --enable-plugins \
   --with-included-ltdl && \
 make install
 
 rm -fr "${PREFIX}/share"
 rm -fr "${PREFIX}/lib/pkgconfig"
-mv "${PREFIX}/lib/dnscrypt-proxy/"*.dll "${PREFIX}/bin/"
-mv "${PREFIX}/lib/dnscrypt-proxy/"*.la "${PREFIX}/bin/"
+mv "${PREFIX}/lib/dnscrypt-proxy/"*.dll "${PREFIX}/"
+mv "${PREFIX}/lib/dnscrypt-proxy/"*.la "${PREFIX}/"
 rm -fr "${PREFIX}/lib"
-cp "${MINGW_PREFIX}/bin/libwinpthread-1.dll" "${PREFIX}/bin/"
-cp "${MINGW_PREFIX}/bin/libgcc_s_dw2-1.dll" "${PREFIX}/bin/"
-cp "${MINGW_PREFIX}/bin/libldns-1.dll" "${PREFIX}/bin/"
-cp "${SODIUM_PREFIX}/bin/libsodium-13.dll" "${PREFIX}/bin/"
+cp "${MINGW_PREFIX}/bin/libwinpthread-1.dll" "${PREFIX}/"
+cp "${MINGW_PREFIX}/bin/libgcc_s_dw2-1.dll" "${PREFIX}/"
+cp "${MINGW_PREFIX}/bin/libldns-1.dll" "${PREFIX}/"
+cp "${SODIUM_PREFIX}/bin/libsodium-13.dll" "${PREFIX}/"
 
 nm "${MINGW_PREFIX}/bin/libldns-1.dll" | \
   fgrep -i libeay &&
-  cp "${MINGW_PREFIX}/bin/"libeay*.dll "${PREFIX}/bin/"
+  cp "${MINGW_PREFIX}/bin/"libeay*.dll "${PREFIX}/"
 
-cp dnscrypt-resolvers.csv "${PREFIX}/bin/"
+cp dnscrypt-resolvers.csv "${PREFIX}/"
