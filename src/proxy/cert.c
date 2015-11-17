1fb3b008b9ffb4ee36076ac96ce3b7b6b49da02e
Frank Denis <github@pureftpd.org>
2015-11-17 01:25:41 +0100

    Check crypto_box_beforenm() return code

--- src/proxy/cert.c
+++ src/proxy/cert.c
@@ -344,8 +344,11 @@ cert_query_cb(int result, char type, int count, int ttl,
                                          proxy_context->resolver_publickey);
         exit(0);
     }
-    dnscrypt_client_init_resolver_publickey(&proxy_context->dnscrypt_client,
-                                            proxy_context->resolver_publickey);
+    if (dnscrypt_client_init_resolver_publickey
+        (&proxy_context->dnscrypt_client, proxy_context->resolver_publickey) != 0) {
+        logger_noformat(proxy_context, LOG_ERR, "Suspicious public key");
+        exit(DNSCRYPT_EXIT_CERT_NOCERTS);
+    }
     dnscrypt_proxy_start_listeners(proxy_context);
     proxy_context->cert_updater.query_retry_step = 0U;
     cert_reschedule_query_after_success(proxy_context);
