fc04209e0b6e5d332042156b262855c7aa63d554
Frank Denis <github@pureftpd.org>
2016-07-31 01:17:59 +0200

    Add a win64 build script

---
+++ dist-build/msys2-win64.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+export CFLAGS="-Os -m64 -mtune=westmere -flto"
+export LDFLAGS="-mtune=westmere -flto"
+export PREFIX="$(pwd)/dnscrypt-proxy-win64"
+export MINGW_PREFIX='/mingw64'
+export SODIUM_PREFIX='/tmp/libsodium-win64'
+
+export CPPFLAGS="${CPPFLAGS} -I${SODIUM_PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${SODIUM_PREFIX}/lib"
+
+./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
+  --host=x86_64-w64-mingw32 \
+  --bindir="$PREFIX" \
+  --sbindir="$PREFIX" \
+  --with-included-ltdl && \
+make install
+
+rm -fr "${PREFIX}/share"
+rm -fr "${PREFIX}/lib/pkgconfig"
+mv "${PREFIX}/lib/dnscrypt-proxy/"*.dll "${PREFIX}/"
+mv "${PREFIX}/lib/dnscrypt-proxy/"*.la "${PREFIX}/"
+rm -fr "${PREFIX}/lib"
+cp "${MINGW_PREFIX}/bin/libwinpthread-1.dll" "${PREFIX}/"
+cp "${MINGW_PREFIX}/bin/libgcc_s_dw2-1.dll" "${PREFIX}/"
+cp "${MINGW_PREFIX}/bin/libldns-1.dll" "${PREFIX}/"
+cp "${SODIUM_PREFIX}/bin/libsodium-18.dll" "${PREFIX}/"
+
+nm "${MINGW_PREFIX}/bin/libldns-1.dll" | \
+  fgrep -i libeay &&
+  cp "${MINGW_PREFIX}/bin/"libeay*.dll "${PREFIX}/"
+
+cp dnscrypt-resolvers.csv "${PREFIX}/"
