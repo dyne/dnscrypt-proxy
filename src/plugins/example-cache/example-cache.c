531961741a94171723939c1b331b4a6e3c24b589
Frank Denis <github@pureftpd.org>
2016-12-22 07:51:22 +0100

    Move a few definitions to example-cache.h

--- src/plugins/example-cache/example-cache.c
+++ src/plugins/example-cache/example-cache.c
@@ -3,36 +3,12 @@
 
 #include <ctype.h>
 #include <stdint.h>
-#include <stdio.h>
+#include <string.h>
 #include <time.h>
 
+#include "example-cache.h"
+
 DCPLUGIN_MAIN(__FILE__);
-
-#define DEFAULT_CACHE_ENTRIES_MAX 50
-#define DEFAULT_MIN_TTL 60
-#define MAX_TTL 86400
-
-#define DNS_MAX_HOSTNAME_LEN 256U
-
-#ifndef putc_unlocked
-# define putc_unlocked(c, stream) putc((c), (stream))
-#endif
-
-typedef struct CacheEntry_ {
-    struct CacheEntry_ *next;
-    uint8_t            *response;
-    uint16_t            response_len;
-    char                qname[DNS_MAX_HOSTNAME_LEN];
-    uint16_t            qtype;
-    time_t              deadline;
-} CacheEntry;
-
-typedef struct Cache_ {
-    CacheEntry *cache_entries;
-    size_t      cache_entries_max;
-    time_t      now;
-    time_t      min_ttl;
-} Cache;
 
 const char *
 dcplugin_description(DCPlugin * const dcplugin)
---
+++ src/plugins/example-cache/example-cache.h
@@ -0,0 +1,33 @@
+#ifndef __EXAMPLE_CACHE_H__
+#define __EXAMPLE_CACHE_H__ 1
+
+#define DEFAULT_CACHE_ENTRIES_MAX 50
+#define DEFAULT_MIN_TTL 60
+#define MAX_TTL 86400
+
+#define DNS_MAX_HOSTNAME_LEN 256U
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct CacheEntry_ {
+    struct CacheEntry_ *next;
+    uint8_t            *response;
+    uint16_t            response_len;
+    char                qname[DNS_MAX_HOSTNAME_LEN];
+    uint16_t            qtype;
+    time_t              deadline;
+} CacheEntry;
+
+typedef struct Cache_ {
+    CacheEntry *cache_entries;
+    size_t      cache_entries_max;
+    time_t      now;
+    time_t      min_ttl;
+} Cache;
+
+#ifndef putc_unlocked
+# define putc_unlocked(c, stream) putc((c), (stream))
+#endif
+
+#endif
