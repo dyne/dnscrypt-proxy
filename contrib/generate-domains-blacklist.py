1139d2e1fe75d37d2deb998f6d6ec5b55b3ec5a2
Frank Denis <github@pureftpd.org>
2016-12-27 23:35:21 +0100

    Add domains-blacklist-local-additions.txt for... local additions

--- contrib/generate-domains-blacklist.py
+++ contrib/generate-domains-blacklist.py
@@ -4,17 +4,24 @@ import re
 import urllib2
 
 
-def parse_blacklist(content):
+def parse_blacklist(content, trusted=False):
+    rx_comment = re.compile(r'^(#|$)')
     rx_u = re.compile(r'^@*\|\|([a-z0-9.-]+[.][a-z]{2,})\^?(\$(popup|third-party))?$')
     rx_l = re.compile(r'^([a-z0-9.-]+[.][a-z]{2,})$')
     rx_h = re.compile(r'^[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\s+([a-z0-9.-]+[.][a-z]{2,})$')
     rx_mdl = re.compile(r'^"[^"]+","([a-z0-9.-]+[.][a-z]{2,})",')
-    rx_b = re.compile(r"^([a-z0-9.-]+[.][a-z]{2,}),.+,[0-9: /-]+,")
+    rx_b = re.compile(r'^([a-z0-9.-]+[.][a-z]{2,}),.+,[0-9: /-]+,')
+    rx_trusted = re.compile(r'^([*a-z0-9.-]+)$')
 
     names = set()
+    rx_set = [rx_u, rx_l, rx_h, rx_mdl, rx_b]
+    if trusted:
+        rx_set = [rx_trusted]
     for line in content.splitlines():
         line = str.lower(str.strip(line))
-        for rx in [rx_u, rx_l, rx_h, rx_mdl, rx_b]:
+        if rx_comment.match(line):
+            continue
+        for rx in rx_set:
             matches = rx.match(line)
             if not matches:
                 continue
@@ -25,12 +32,15 @@ def parse_blacklist(content):
 
 def blacklist_from_url(url):
     req = urllib2.Request(url)
+    trusted = False
+    if req.get_type() == "file":
+        trusted = True
     response = urllib2.urlopen(req, timeout=10)
-    if response.getcode() != 200:
+    if trusted is False and response.getcode() != 200:
         print("# HTTP return code: {}".format(response.getcode()))
         return set()
     content = response.read()
-    return parse_blacklist(content)
+    return parse_blacklist(content, trusted)
 
 
 def name_cmp(name):
