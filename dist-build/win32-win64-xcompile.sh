c110daf432c757dfabf7379f541e0cd33000d543
Frank Denis <github@pureftpd.org>
2016-12-31 23:08:35 +0100

    Keep the frame pointers to get nicer backtraces on Windows

--- dist-build/win32-win64-xcompile.sh
+++ dist-build/win32-win64-xcompile.sh
@@ -140,12 +140,12 @@ setup
 
 fetch_src
 
-export CFLAGS="-Os -fomit-frame-pointer -m64 -mtune=westmere"
+export CFLAGS="-Os -m64 -mtune=westmere"
 export LDFLAGS="-mtune=westmere -static-libgcc -Wl,--dynamicbase -Wl,--high-entropy-va -Wl,--nxcompat -static-libgcc"
 export TARGET=x86_64-w64-mingw32
 compile dnscrypt-proxy-win64
 
-export CFLAGS="-Os -fomit-frame-pointer -m32 -march=pentium3 -mtune=core2"
+export CFLAGS="-Os -m32 -march=pentium3 -mtune=core2"
 export LDFLAGS="-march=pentium3 -static-libgcc -Wl,--dynamicbase -Wl,--nxcompat -static-libgcc"
 export TARGET=i686-w64-mingw32
 compile dnscrypt-proxy-win32
