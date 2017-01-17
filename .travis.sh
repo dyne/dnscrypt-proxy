335b654d8f1534d09943ccc2a9b792849a00c6fc
Frank Denis <github@pureftpd.org>
2017-01-17 15:33:24 +0100

    Switch Travis builds to multiarch/alpine

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,18 @@
 #! /bin/sh
 
-set -e
+cd /workdir
+
+apk --update upgrade
+apk add alpine-sdk
+apk add coreutils
+apk add ldns-dev
 
 cd libsodium
-./configure --disable-dependency-tracking
-sudo make -j$(nproc) install
-sudo ldconfig
+./configure --disable-dependency-tracking --prefix=/usr
+make clean
+make -j$(nproc) check
+make -j$(nproc) install
+/sbin/ldconfig
 cd ..
 ./configure --disable-dependency-tracking --enable-debug
 make clean
