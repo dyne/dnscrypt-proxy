1b1789502b7f8ee5868f67d50a6ce1259e66079b
Frank Denis <github@pureftpd.org>
2017-01-21 02:54:08 +0100

    Use a random resolver in the tests

--- .travis.sh
+++ .travis.sh
@@ -22,12 +22,12 @@ make clean
 make -j$(nproc) check
 make -j$(nproc) install
 
-/usr/local/sbin/dnscrypt-proxy -t 60 -R dnscrypt.org-fr \
+/usr/local/sbin/dnscrypt-proxy -t 60 -R random \
   --plugin=libdcplugin_example.so \
   --plugin=libdcplugin_example_cache.so \
   --plugin=/usr/local/lib/dnscrypt-proxy/libdcplugin_example_ldns_aaaa_blocking.so
 
-echo 'ResolverName dnscrypt.org-fr' > /tmp/dnscrypt-proxy.conf
+echo 'ResolverName random' > /tmp/dnscrypt-proxy.conf
 echo 'LocalCache yes' >> /tmp/dnscrypt-proxy.conf
 echo 'BlockIPv6 yes' >> /tmp/dnscrypt-proxy.conf
 echo 'Plugin libdcplugin_example.so' >> /tmp/dnscrypt-proxy.conf
