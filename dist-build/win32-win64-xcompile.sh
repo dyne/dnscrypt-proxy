678db7db7e2e1147a825914684ab5649fc1c0484
Frank Denis <github@pureftpd.org>
2017-05-07 14:26:27 +0200

    Compile a stable version of LibreSSL

--- dist-build/win32-win64-xcompile.sh
+++ dist-build/win32-win64-xcompile.sh
@@ -25,7 +25,7 @@ fetch_src() {
 
   cd $SRCDEPS_DIR
   (
-    git clone --recursive https://github.com/libressl-portable/portable libressl
+    git clone --recursive https://github.com/libressl-portable/portable libressl --branch=v2.5.4
     cd libressl
     ./autogen.sh
   )
